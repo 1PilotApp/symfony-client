7483c3c49a7018ddeb09ae8cf55ea01862a2cf09
Francesco Abeni <f.abeni@gibilogic.com>
2018-10-03 12:35:21 +0200

    Remove unused deps and fix tests config

--- tests/app/AppKernel.php
+++ tests/app/AppKernel.php
@@ -9,6 +9,7 @@ class AppKernel extends Kernel
     {
         return [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
             new \OnePilot\ClientBundle\OnePilotClientBundle(),
         ];
     }
