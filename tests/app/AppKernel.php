a6e27355da26dcfb2bc30129920f809bdb82a6de
Christophe Vuagniaux <christophe@inetis.ch>
2022-12-09 16:32:44 +0100

    Fix tests

--- tests/app/AppKernel.php
+++ tests/app/AppKernel.php
@@ -5,7 +5,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    public function registerBundles()
+    public function registerBundles(): iterable
     {
         return [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
