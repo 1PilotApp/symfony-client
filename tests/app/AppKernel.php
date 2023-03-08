60e45f2a30f54e110fda6f86ae07233abe040999
Christophe Vuagniaux <christophe@inetis.ch>
2023-03-08 12:30:36 +0100

    Clean & update dev dependencies

--- tests/app/AppKernel.php
+++ tests/app/AppKernel.php
@@ -9,8 +9,7 @@ class AppKernel extends Kernel
     {
         return [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \OnePilot\ClientBundle\OnePilotClientBundle(),
+            new OnePilot\ClientBundle\OnePilotClientBundle(),
         ];
     }
 
